cd11de887c1548c4fcc3bf8ea8a6289b1486c9fd
waseprisca0 <117096121+waseprisca0@users.noreply.github.com>
2023-10-21 01:19:48 +0200

    Update 3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""Script that starts a Flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Comment"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Comment"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_route(text):
+    """Comment"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def text_route_python(text="is cool"):
+    """Comment"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
